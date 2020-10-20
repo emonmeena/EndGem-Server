4559e6b0c185dd7e1d591bbae457a5b966627d64
maayami <mayank16meena@gmail.com>
2020-10-20 23:00:03 +0530

    setup api endpoints and modal

--- first_app/urls.py
+++ first_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('topics/', views.topics, name = 'topicsFirstApp'),
-    # path('topics/<id>', views.topicsDelete, name = 'topicsFirstApp'),
+    path('topics/', views.topic_names, name = 'topic names'),
+    path('topics/<pk>', views.topic_details, name = 'topic details'),
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,16 +1,44 @@
+from decimal import Context
 from django.http import request
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from django.http import HttpResponse
-from .models import Topic, Accessrecord, Webpage 
+from rest_framework import status
+from rest_framework.status import HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST
+from .models import Topic
 from .serializers import *
 # Create your views here.
 
 @api_view(['GET', 'POST'])
-def topics(request):
+def topic_names(request):
     if request.method == 'GET':
         data = Topic.objects.all()
         serializedData = TopicSerializer(data, context={'request': request}, many=True)
 
         return Response(serializedData.data)
 
+    elif request.method == 'POST':
+        serializedData = TopicSerializer(data = request.data)
+        if serializedData.is_valid():
+            serializedData.save()
+            return Response(status=status.HTTP_201_CREATED)
+
+        return Response(serializedData.errors, status= status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT', 'DELETE'])
+def topic_details(request, pk):
+    try:
+        topic_name = Topic.objects.get(pk=pk)
+    except Topic.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializedData = TopicSerializer(topic_name, data=request.data, context={'request': request})
+        if serializedData.is_valid():
+            serializedData.save()
+            return Response(status=HTTP_204_NO_CONTENT)
+
+        return Response(serializedData.errors, status=HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        topic_name.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)                    
